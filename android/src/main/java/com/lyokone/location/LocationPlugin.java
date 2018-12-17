88134714c957375fc30d731dd39ce5e8082dfc14
Somnibyte <somnibyte@gmail.com>
2018-12-17 16:35:54 -0500

    Fix variable error.

--- android/src/main/java/com/lyokone/location/LocationPlugin.java
+++ android/src/main/java/com/lyokone/location/LocationPlugin.java
@@ -58,7 +58,7 @@ public class LocationPlugin implements MethodCallHandler, StreamHandler {
     private static final int REQUEST_CHECK_SETTINGS = 0x1;
     private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
     private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
-
+    private static MethodChannel channel;
     private FusedLocationProviderClient mFusedLocationClient;
     private SettingsClient mSettingsClient;
     private LocationRequest mLocationRequest;
@@ -220,7 +220,7 @@ public class LocationPlugin implements MethodCallHandler, StreamHandler {
      * Plugin registration.
      */
     public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), METHOD_CHANNEL_NAME);
+        channel = new MethodChannel(registrar.messenger(), METHOD_CHANNEL_NAME);
         LocationPlugin locationPlugin = new LocationPlugin(registrar.activity());
         channel.setMethodCallHandler(locationPlugin);
         registrar.addRequestPermissionsResultListener(locationPlugin.getPermissionsResultListener());
