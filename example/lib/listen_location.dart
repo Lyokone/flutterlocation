84c591a77e18862953a9a5bfbaee4e1f26663f2c
Guillaume Bernos <guillaume.bernos@devoteamgcloud.com>
2020-02-22 17:57:51 +0100

    Update analyze CI

--- example/lib/listen_location.dart
+++ example/lib/listen_location.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:location/location.dart';
 
 class ListenLocationWidget extends StatefulWidget {
